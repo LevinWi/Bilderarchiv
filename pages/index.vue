5dbbb3d216b43ef3a19bfbf8c68363b36275c304
MIchael Brechbuehl <michael.brechbuehl@students.ffhs.ch>
2021-12-10 10:12:26 +0100

    geile commit

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,26 @@
     <div class="wrapper">
       <Frontpage/>
       <div class="grid-list">
+
         <div class="column1">
           <figure v-for="item, index in images" :key="index" :class="item.size">
-          <img :src="`/pics/${item.path}`" alt="">
-          <p class="image-text-c1">{{item.size}}, {{item.hasChildren}}</p>
+            <img 
+              @mouseover= "handleMouseEnter(`left_${index}`)"
+              @mouseleave="hoveredElement=null"
+              :src="`/pics/${item.path}`" alt="">
+            <p class="image-text-c1" v-if="hoveredElement===`left_${index}`">
+              {{item.size}}, {{item.hasChildren}}
+            </p>
           </figure>
         </div>
+
         <div class="column2">
           <figure v-for="item, index in images" :key="index" :class="item.size">
           <img :src="`/pics/${item.path}`" alt="">
           <p class="image-text-c2">{{item.size}}, {{item.hasChildren}}</p>
           </figure>
         </div>
+
       </div>
     </div>
 </template>
@@ -26,7 +34,15 @@ export default {
       title: 'my things',
       images: DBimages,
       color: '',
+      hoveredElement: false,
     }
   },
+  methods: {
+    handleMouseEnter(index){
+      this.hoveredElement=index
+      console.log(index)
+    }
+    }
+  
 }
 </script>
