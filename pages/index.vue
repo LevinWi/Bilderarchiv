b7c28b553553d629f09a3bbc18bd17c74d7ce852
LevinWi <levin.witjes@gmail.com>
2021-12-10 11:58:14 +0100

    API IS DONE

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,23 @@
       <div class="grid-list">
 
         <div class="column1">
-          <figure v-for="item, index in images" :key="index" :class="item.size">
+          <figure v-for="item, PhotoId in images" :key="PhotoId" :class="item.size">
             <img 
-              @mouseover= "handleMouseEnter(`left_${index}`)"
+              v-if="item.PhotoId%2==0"
+              @mouseover= "handleMouseEnter(`left_${PhotoId}`)"
               @mouseleave="hoveredElement=null"
-              :src="`/pics/${item.path}`" alt="">
-            <p class="image-text-c1" v-if="hoveredElement===`left_${index}`">
-              {{item.size}}, {{item.hasChildren}}
+              :src="item.Link" :alt="item.AltText">
+            <p class="image-text-c1" v-if="hoveredElement===`left_${PhotoId}`">
+              Titel: {{item.TextTitle}} <br> {{item.Description}}
             </p>
           </figure>
         </div>
 
         <div class="column2">
-          <figure v-for="item, index in images" :key="index" :class="item.size">
-            <img 
-              @mouseover= "handleMouseEnter(`right_${index}`)"
-              @mouseleave="hoveredElement=null"
-              :src="`/pics/${item.path}`" alt="">
-            <p class="image-text-c2" v-if="hoveredElement===`right_${index}`">
-              {{item.size}}, {{item.hasChildren}}
+          <figure v-for="item, PhotoId in images" :key="PhotoId" :class="item.size">
+            <img v-if="item.PhotoId%2==1" @mouseover= "handleMouseEnter(`right_${PhotoId}`)" @mouseleave="hoveredElement=null" :src="item.Link" :alt="item.AltText">
+            <p class="image-text-c2" v-if="hoveredElement===`right_${PhotoId}`">
+               Titel: {{item.TextTitle}} <br> {{item.Description}}
             </p>
           </figure>
         </div>
@@ -32,21 +30,28 @@
 </template>
 
 <script>
-const DBimages = require('~/assets/data/images.json')
+// const DBimages = require('~/assets/data/images.json')
+
 export default {
   data() {
     return {
       title: 'my things',
-      images: DBimages,
+      images: [],
       color: '',
       hoveredElement: false,
     }
+  }, 
+  async created() {
+    const DBimages = await this.$axios.get('https://webex-groupc.azurewebsites.net/api/Photo')
+    
+    this.images = DBimages.data
+    console.log(DBimages.data[1].Link)
   },
   methods: {
     handleMouseEnter(index){
       this.hoveredElement=index
     }
-    }
+  }
   
 }
 </script>
